6d7f29bc998b6a0c510525a7618e253ec5103305
Grant <zukeru@gmail.com>
2017-04-04 22:00:53 -0600

    updated the userdata to install nexus for test deployment

--- userdata.sh
+++ userdata.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
-sudo yum install nginx -y
-sudo echo "$TIMESTAMP" > /home/ec2-user/startup.log
-sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
-sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
-sudo yum install jenkins -y
-sudo yum remove java -y
-sudo yum install java-1.7.0-openjdk-devel -y
-sudo chkconfig jenkins on
-sudo service jenkins start
-sudo echo "THE-AUTOMATION-IS-DONE"
-sudo cat /var/lib/jenkins/secrets/initialAdminPassword > /home/ec2-user/jenkinspwd
+sudo yum update -y
+sudo yum install java-1.8.0-openjdk.x86_64
+sudo mkdir /app && cd /app
+sudo wget https://sonatype-download.global.ssl.fastly.net/nexus/3/nexus-3.0.2-02-unix.tar.gz
+sudo tar -xvf nexus-3.0.2-02-unix.tar.gz
+sudo mv nexus-3.0.2-02 nexus
+sudo adduser nexus
+sudo chown -R nexus:nexus /app/nexus
+echo 'run_as_user="nexus"' >> /app/nexus/bin/nexus.rc
+echo "-Xms1200M" >> /app/nexus/bin/nexus.vmoptions
+echo "-Xmx1200M" >> /app/nexus/bin/nexus.vmoptions
+echo "-XX:+UnlockDiagnosticVMOptions" >> /app/nexus/bin/nexus.vmoptions
+echo "-XX:+UnsyncloadClass" >> /app/nexus/bin/nexus.vmoptions
+echo "-Djava.net.preferIPv4Stack=truer" >> /app/nexus/bin/nexus.vmoptions
+echo "-Dkaraf.home=." >> /app/nexus/bin/nexus.vmoptions
+echo "-Dkaraf.base=." >> /app/nexus/bin/nexus.vmoptions
+echo "-Dkaraf.etc=etc" >> /app/nexus/bin/nexus.vmoptions
+echo "-Djava.util.logging.config.file=etc/java.util.logging.properties" >> /app/nexus/bin/nexus.vmoptions
+echo "-Dkaraf.data=/nexus/nexus-data" >> /app/nexus/bin/nexus.vmoptions
+echo "-Djava.io.tmpdir=data/tmp" >> /app/nexus/bin/nexus.vmoptions
+echo "-Dkaraf.startLocalConsole=false" >> /app/nexus/bin/nexus.vmoptions
+
+sudo ln -s /app/nexus/bin/nexus /etc/init.d/nexus
+sudo chkconfig --add nexus
+sudo chkconfig --levels 345 nexus on
+sudo service nexus start
